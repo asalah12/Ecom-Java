9e032815f1473b38fa0352ebd8ace82340e6732a
asalah19 <asalah.massalha12@gmail.com>
2023-03-09 20:23:15 +0200

    first commit

---
+++ src/lesson_01/ClassTasks/MyFirstTask.java
@@ -0,0 +1,14 @@
+package lesson_01.ClassTasks;
+
+public class MyFirstTask {
+    public static void main(String[] args) {
+        System.out.print("Asalah ");
+        System.out.println("Massalha");
+        System.out.println(" ");
+        System.out.println("A'rara");
+        System.out.println(" ");
+        System.out.println(1996);
+
+
+    }
+}
---
+++ src/lesson_01/firstclass.java
@@ -0,0 +1,9 @@
+package lesson_01;
+
+public class firstclass {
+    public static void main(String[] args) {
+        System.out.println("Asalah Massalha");
+        System.out.println(" ");
+        System.out.print(16);
+    }
+}
