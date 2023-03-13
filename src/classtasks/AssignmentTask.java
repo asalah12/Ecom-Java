c9cd142b7fe820e74db3cd4a8625bcd7126e2613
asalah19 <asalah.massalha12@gmail.com>
2023-03-13 20:30:18 +0200

    Assignent

---
+++ src/classtasks/AssignmentTask.java
@@ -0,0 +1,10 @@
+package classtasks;
+
+public class AssignmentTask {
+    public static void main(String[] args) {
+        int a=10;
+       System.out.println("a =" +  (((((((a+90)-25)/3)%10)*-3)+1)-1));
+
+
+    }
+}
