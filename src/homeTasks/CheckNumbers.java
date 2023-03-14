5f0b35702f5a4b5aca1868972e007cbecdcbe706
asalah19 <asalah.massalha12@gmail.com>
2023-03-15 00:19:53 +0200

    CheckNumber

---
+++ src/homeTasks/CheckNumbers.java
@@ -0,0 +1,28 @@
+package homeTasks;
+
+public class CheckNumbers {
+    public static void main(String[] args) {
+        System.out.println("0 => " + (0 % 2 == 0));
+        //if (0 % 2 == 0) {System.out.println("  0 => even") ;}
+        //else {System.out.println("  0 => odd");}
+
+        System.out.println("22 => " + (22 % 2 == 0));
+       // if (22 % 2 == 0) {System.out.println("  22 => even") ;}
+       // else {System.out.println("  22 => odd");}
+
+        System.out.println("55 => " + (55 % 2 == 0));
+        //if (55 % 2 == 0) {System.out.println("  55 => even") ;}
+       // else {System.out.println("  55 => odd");}
+
+        System.out.println("25 => " + (25 % 2 == 0));
+        //if (25 % 2 == 0) {System.out.println("  25 => even") ;}
+       // else {System.out.println("  25 => odd");}
+
+        System.out.println("10 => " + (10 % 2 == 0));
+
+        //if (10 % 2 == 0) {System.out.print("  10 => even") ;}
+        //else {System.out.println("  10 => odd");}
+
+        }
+    }
+
