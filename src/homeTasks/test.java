65d4e5195db27335079fb05fc163e9d8327cc3d7
asalah19 <asalah.massalha12@gmail.com>
2023-03-16 14:41:49 +0200

    CheckNumber

---
+++ src/homeTasks/test.java
@@ -0,0 +1,10 @@
+package homeTasks;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("10>6= " +(10>6) );
+        System.out.println("0 =>" + (0%2==0));
+        System.out.println("5 =>" + (5%2==0));
+
+    }
+}
