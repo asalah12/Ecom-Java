64923e2a940a00be22bd84807fa283a5d2ba1e90
asalah19 <asalah.massalha12@gmail.com>
2023-03-17 21:42:53 +0200

    credentialsTask

--- src/homeTasks/CredentialsTask.java
+++ src/homeTasks/CredentialsTask.java
@@ -8,7 +8,6 @@ public class CredentialsTask {
         String UserName= scanner.nextLine();
         System.out.println("enter your password please:");
         String Password= scanner.nextLine();
-      //  String result;
         System.out.println( UserName.equals(Password)? "wellcome "+UserName : "there is not such user");
     }
 }
